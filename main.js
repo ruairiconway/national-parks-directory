570e1d91140f235e3dddf0f739184c39c58c2944
Ruairi Conway <hi@ruairiconway.com>
2020-10-30 12:01:28 -0400

    tweaked form id's

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function getParkList(searchStateCode, searchMaxNum) {
+    var myHeaders = new Headers();
+    myHeaders.append("X-Api-Key", myApiKey);
+    myHeaders.append("Cookie", "AWSALB=HSsMCw5PQ/VBAEpwO5FcpdhbU7LNODA620eOkhtSYIk0h6Qhge6apaPjS4VH2jwmo8UZINK0nQi3Uj1Zle30Kp7UMvlM6X6y6fGZiS/DGfH4oni5+n5OG3+6LBmm; AWSALBCORS=HSsMCw5PQ/VBAEpwO5FcpdhbU7LNODA620eOkhtSYIk0h6Qhge6apaPjS4VH2jwmo8UZINK0nQi3Uj1Zle30Kp7UMvlM6X6y6fGZiS/DGfH4oni5+n5OG3+6LBmm");
+    
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    
+    fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${searchStateCode}&limit=${searchMaxNum}`, requestOptions)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+        .catch(error => console.log('error', error));
+}
+
+function watchForm() {
+    $('#search-form').submit(event => {
+        event.preventDefault();
+        let searchStateCode = $('#js-search-state').val();
+        let searchMaxNum = $('#js-search-num').val();
+        getParkList(searchStateCode, searchMaxNum);
+    });
+}
+
+$(watchForm);
