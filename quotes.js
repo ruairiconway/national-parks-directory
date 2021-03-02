7171b17ad962d734302b45e467ee669f78d57590
Ruairi Conway <hi@ruairiconway.com>
2021-03-02 11:29:53 -0500

    changed from .json to .js

---
+++ quotes.js
@@ -0,0 +1,48 @@
+const quotes = [
+    {
+        author: "Franklin D. Roosevelt",
+        title: "U.S. President",
+        quote: "There is nothing so American as our national parks…. The fundamental idea behind the parks…is that the country belongs to the people."
+    },
+    {
+        author: "John Muir",
+        title: "Author",
+        quote: "Everybody needs beauty as well as bread, places to play in and pray in, where nature may heal and give strength to body and soul alike."
+    },
+    {
+        author: "J. Horace McFarland",
+        title: "Pres., American Civil Assn.",
+        quote: "The parks are the Nation’s pleasure grounds and the Nation’s restoring places…. The national parks…are an American idea; it is one thing we have that has not been imported."
+    },
+    {
+        author: "Henry David Thoreau",
+        title: "Author",
+        quote: "I have a room all to myself. It is nature."
+    },
+    {
+        author: "Lyndon B. Johnson",
+        title: "U.S. President",
+        quote: "If future generations are to remember us with gratitude rather than contempt, we must leave them something more than the miracles of technology. We must leave them a glimpse of the world as it was in the beginning."
+    },
+    {
+        author: "Theodore Roosevelt",
+        title: "U.S. President",
+        quote: "There are no words that can tell the hidden spirit of the wilderness, that can reveal its mystery, its melancholy and its charm. The nation behaves well if it treats the natural resources as assets which it must turn over to the next generation increased and not impaired in value."
+    },
+    {
+        author: "John F. Kennedy",
+        title: "U.S. President",
+        quote: "It is the course of wisdom to set aside an ample portion of our natural resources as national parks and reserves, thus ensuring that future generations may know the majesty of the earth as we know it today."
+    },
+    {
+        author: "Mollie Beattie",
+        title: "Director, U.S. Fish and Wildlife Service",
+        quote: "What a country chooses to save is what a country chooses to say about itself."
+    },
+    {
+        author: "Henry David Thoreau",
+        title: "Author",
+        quote: "I have a room all to myself. It is nature."
+    }
+]
+
